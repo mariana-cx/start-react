67355e83d6af389f792c7f0f598e6d0aa6be7a5c
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-07-25 18:26:35 -0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
+import { useState } from 'react';
 import './App.css';
-import { Card } from './components/Card';
-// import components 
-import { InputData } from './components/InputDatas';
-import { MyName } from './components/MyName';
+import { useEffect } from 'react';
+
+let people = ['João Henrique Xavier', 'Lebron', 'Aaliyah', 'Diamond', 'Dominique', 'Aliyah'];
 
 export function AppLearnReact() {
+  const [names, setNames] = useState(people)
+  const [currentName, setCurrentName] = useState("")
+  useEffect(() => {
+    setCurrentName("")
+  }, [names])
+  function removeName(arrNames, person) {
+    let result = arrNames.filter(e => e != person)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* imported components */}
-        <Card />
-      </header>
+    return result
+  }
+  console.log(people)
+  return (<div className="App">
+    <div className="App-header">
+      <span>
+        <input value={currentName} onChange={(e) => {
+          setCurrentName(e.target.value);
+          console.log("Current name is: ", currentName);
+        }} />
+        <button className="inputButton" onClick={() => {
+          setNames([...names, currentName]);
+          setCurrentName("");
+        }}><strong>Add</strong></button>
+      </span>
+
+      {names.map(e => <p onClick={() => setNames(removeName(names, e))} key={e}>{e}</p>)}
+
     </div>
+    <div className='Box'>
+      
+    </div>
+  </div >
+  
   );
 }
-
-
-
